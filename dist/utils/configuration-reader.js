b0e440383c67e9a977b931fb96119038f6e2bdd7
Sebastian Landwehr <info@dword-design.de>
2020-01-22 11:00:14 +0100

    compile

--- dist/utils/configuration-reader.js
+++ dist/utils/configuration-reader.js
@@ -36,28 +36,16 @@ function createParsersObject(parsersFromCli) {
 }
 
 function getCliArgs(args, version) {
-  return (0, _yargs.default)(args).usage('Usage: $0 [DIRECTORY]').boolean(['ignore-bin-package', 'skip-missing']).describe('ignore-bin-package', 'Ignore package with bin entry').describe('skip-missing', 'Skip calculation of missing dependencies').describe('json', 'Output results to JSON').describe('ignores', 'Comma separated package list to ignore').describe('ignore-dirs', 'Comma separated folder names to ignore').describe('prod-dependency-matches', 'Comma separated matchers supporting production dependencies').describe('parsers', 'Comma separated glob:parser pair list').describe('detectors', 'Comma separated detector list').describe('specials', 'Comma separated special parser list').version('version', 'Show version number', version).help('help', 'Show this help message').coerce(['ignores', 'ignore-dirs', 'detectors', 'specials', 'prod-dependency-matches'], parseCsvArray).coerce('parsers', parsersStr => {
+  return (0, _yargs.default)(args).usage('Usage: $0 [DIRECTORY]').boolean(['ignore-bin-package', 'skip-missing']).describe('config', 'A config file to be parsed').describe('ignore-bin-package', 'Ignore package with bin entry').describe('skip-missing', 'Skip calculation of missing dependencies').describe('json', 'Output results to JSON').describe('ignores', 'Comma separated package list to ignore').describe('ignore-dirs', 'Comma separated folder names to ignore').describe('prod-dependency-matches', 'Comma separated matchers supporting production dependencies').describe('parsers', 'Comma separated glob:parser pair list').describe('detectors', 'Comma separated detector list').describe('specials', 'Comma separated special parser list').version('version', 'Show version number', version).help('help', 'Show this help message').coerce(['ignores', 'ignore-dirs', 'detectors', 'specials', 'prod-dependency-matches'], parseCsvArray).coerce('parsers', parsersStr => {
     const parsers = parseCsvArray(parsersStr);
     return createParsersObject(parsers);
   });
 }
-/* istanbul ignore next */
 
-
-function returnNull() {
-  return null;
-}
-
-async function getRCFileConfiguration(moduleName) {
+async function getRCFileConfiguration(moduleName, filename) {
   try {
-    const configFileExplorer = (0, _cosmiconfig.default)(moduleName, {
-      // this prevents cosmiconfig from picking up .js configuration files. "null" means no file was found.
-      // Gotta extract `() => null` into a function to be able to ignore the line from the code coverage count.
-      loaders: {
-        '.js': returnNull
-      }
-    });
-    const findings = await configFileExplorer.search();
+    const configFileExplorer = (0, _cosmiconfig.default)(moduleName);
+    const findings = await (filename !== undefined ? configFileExplorer.load(filename) : configFileExplorer.search());
     return !findings || findings.isEmpty ? {} : convertObjectToCamelCase(findings.config);
   } catch (error) {
     // It's not documented in cosmiconfig's documentation,
@@ -67,8 +55,8 @@ async function getRCFileConfiguration(moduleName) {
 }
 
 async function getConfiguration(args, moduleName, version) {
-  const rcConfig = await getRCFileConfiguration(moduleName);
   const cliConfig = getCliArgs(args, version);
+  const rcConfig = await getRCFileConfiguration(moduleName, cliConfig.argv.config);
   return { ...rcConfig,
     ...cliConfig.argv
   };
